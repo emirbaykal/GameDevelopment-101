876b9e9975485eeea89dfdd921af634be658aaa2
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2026-02-13 21:33:28 +0300

    Interface Segregation Principle

---
+++ Assets/GameDevelopment-101/SOLID_Principles/InterfaceSegregation/Interfaces/ExampleInterfaces.cs
@@ -0,0 +1,22 @@
+namespace GameDevelopment_101.SOLID_Principles.InterfaceSegregation.Interfaces
+{
+    public interface IMovable
+    {
+        
+    }
+
+    public interface IAttackable
+    {
+        
+    }
+
+    public interface IMagicUser
+    {
+        
+    }
+
+    public interface ITrader
+    {
+        
+    }
+}
