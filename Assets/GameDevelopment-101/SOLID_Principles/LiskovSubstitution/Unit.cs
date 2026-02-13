61010b683d9453b7257d639742d441a7c510c222
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2026-02-13 19:37:58 +0300

    Liskov Substitution

--- Assets/GameDevelopment-101/SOLID_Principles/LiskovSubstitution/Unit.cs
+++ Assets/GameDevelopment-101/SOLID_Principles/LiskovSubstitution/Unit.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+using GameDevelopment_101.SOLID_Principles.LiskovSubstitution.Interfaces;
 using UnityEngine;
 
 namespace GameDevelopment_101.SOLID_Principles.LiskovSubstitution
 {
+    public class MainCaller
+    {
+        static void Main(string[] args)
+        {
+            List<Unit> allUnits = new List<Unit>();
+            foreach (var unit in allUnits)
+            {
+                // For Move
+                if (unit is IMovable movableUnit)
+                {
+                    movableUnit.Move();
+                }
+                // For Swim
+                if (unit is ISwimmable swimmableUnit)
+                {
+                    swimmableUnit.Swim();
+                }
+            }
+        }
+    }
+    
     public class Unit : MonoBehaviour
     {
         public int health;
